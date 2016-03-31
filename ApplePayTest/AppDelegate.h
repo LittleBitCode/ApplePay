bbaa933a4328a58351bd2f47ee9ae0c24dc126b4
LittleBitCode <littlebit@littleBitdeMac-mini.local>
2016-03-31 08:47:50 +0800

    ApplePay
    ApplePayDemo

---
+++ ApplePayTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayTest
+//
+//  Created by littleBit on 16/3/7.
+//  Copyright © 2016年 littleBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayTest
+//
+//  Created by littleBit on 16/3/7.
+//  Copyright © 2016年 littleBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
